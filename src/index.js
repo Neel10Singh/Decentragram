8bc98c2b4f3a036cf1e921be86ed738051c4e17a
Neel10Singh <neelaksh10singh@gmail.com>
2024-02-05 23:07:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../src/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root');
 const root = createRoot(container);
